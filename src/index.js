f012d2dfc32e4f7f76482780839f3748eaaa2162
Stoodkev <quentin.correa@gmail.com>
2019-12-13 23:01:14 +0800

    serve https

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 require("dotenv").config();
-
+const https = require("https");
+const fs = require("fs");
 const app = require("./app");
 const port = process.env.PORT || 3000;
 
 // Listening
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync(
+        "/etc/letsencrypt/live/api.steemkeychain.com/privkey.pem",
+        "utf8"
+      ),
+      cert: fs.readFileSync(
+        "/etc/letsencrypt/live/api.steemkeychain.com/cert.pem",
+        "utf8"
+      ),
+      ca: fs.readFileSync(
+        "/etc/letsencrypt/live/api.steemkeychain.com/chain.pem",
+        "utf8"
+      )
+    },
+    app
+  )
+  .listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
