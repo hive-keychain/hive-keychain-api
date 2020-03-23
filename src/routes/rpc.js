ed5338e1cff2ee54392ce366f5e824daaf7bf971
Stoodkev <quentin.correa@gmail.com>
2020-03-23 15:10:38 +0800

    update

--- src/routes/rpc.js
+++ src/routes/rpc.js
@@ -7,7 +7,7 @@ router.get("/rpc", async (req, res) => {
   res.send({rpc: config.rpc});
 });
 
-router.get("hive/rpc", async (req, res) => {
+router.get("/hive/rpc", async (req, res) => {
   res.send({rpc: config.hive_rpc});
 });
 
