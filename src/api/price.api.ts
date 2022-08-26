76d35cd048c472ec31c3cebd606669b8a930f8e4
Cedric Gui <cedric.guillas78@gmail.com>
2022-08-26 14:56:29 +0800

    removed log

--- src/api/price.api.ts
+++ src/api/price.api.ts
@@ -2,7 +2,6 @@ import { Express } from "express";
 import { PriceLogic } from "../logic/price.logic";
 
 const setupGetPriceApi = (app: Express) => {
-  console.log("setting up hive prices");
   app.get("/hive/v2/price", async (req, res) => {
     res.status(200).send(await PriceLogic.getPrices());
   });
