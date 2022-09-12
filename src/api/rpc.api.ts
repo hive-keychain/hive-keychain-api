f343125ed03d613a6e30fbc65a578208fc6d1d86
stoodkev <quentin.correa@gmail.com>
2022-09-12 15:45:50 +0800

    fix rpc api + add thunder api tests

--- src/api/rpc.api.ts
+++ src/api/rpc.api.ts
@@ -3,20 +3,14 @@ import { Config } from "../config";
 
 // RPC from env
 
-const setupGetRpcApi = (app: Express) => {
-  app.get("/hive/rpc", async (req, res) => {
-    res.send({ rpc: Config.rpc });
-  });
-};
 const setupGetHiveRpcApi = (app: Express) => {
-  app.get("/hive/hive/rpc", async (req, res) => {
+  app.get("/hive/rpc", async (req, res) => {
     res.send({ rpc: Config.hive_rpc });
   });
 };
 
 const setupApis = (app: Express) => {
   setupGetHiveRpcApi(app);
-  setupGetRpcApi(app);
 };
 
 export const RpcApi = {
