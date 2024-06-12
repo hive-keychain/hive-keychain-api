4798bd2f2e5cbb30caf08c9dd75005d05b734052
stoodkev <quentin.correa@gmail.com>
2024-06-12 09:50:21 +0800

    update ecosystem path file

--- src/logic/ecosystem/ecosystem.logic.ts
+++ src/logic/ecosystem/ecosystem.logic.ts
@@ -50,28 +50,28 @@ const getOrderedDapps = (dApps: dApp[]) => {
 
 const saveNewDapp = (newDapp: any, chain: string) => {
   const jsonString = fs.readFileSync(
-    path.join(__dirname, `${chain}-dapps.json`),
+    path.join(__dirname, `../../../json/${chain}-dapps.json`),
     "utf-8"
   );
   const dapps = JSON.parse(jsonString);
   const id = Math.max(...dapps.map((d) => d.id));
   dapps.push({ ...newDapp, id: id + 1 });
   fs.writeFileSync(
-    path.join(__dirname, `${chain}-dapps.json`),
+    path.join(__dirname, `../../../json/${chain}-dapps.json`),
     JSON.stringify(dapps)
   );
 };
 
 const editDapp = (dapp: any, chain: string) => {
   const jsonString = fs.readFileSync(
-    path.join(__dirname, `${chain}-dapps.json`),
+    path.join(__dirname, `../../../json/${chain}-dapps.json`),
     "utf-8"
   );
   const dapps = JSON.parse(jsonString).filter((d) => d.id !== dapp.id);
   dapps.push(dapp);
   try {
     fs.writeFileSync(
-      path.join(__dirname, `${chain}-dapps.json`),
+      path.join(__dirname, `../../../json/${chain}-dapps.json`),
       JSON.stringify(dapps)
     );
   } catch (err) {
@@ -81,13 +81,13 @@ const editDapp = (dapp: any, chain: string) => {
 
 const deleteDapp = (dapp: any, chain: string) => {
   const jsonString = fs.readFileSync(
-    path.join(__dirname, `${chain}-dapps.json`),
+    path.join(__dirname, `../../../json/${chain}-dapps.json`),
     "utf-8"
   );
   const dapps = JSON.parse(jsonString).filter((d) => d.id !== dapp.id);
   try {
     fs.writeFileSync(
-      path.join(__dirname, `${chain}-dapps.json`),
+      path.join(__dirname, `../../../json/${chain}-dapps.json`),
       JSON.stringify(dapps)
     );
   } catch (err) {
