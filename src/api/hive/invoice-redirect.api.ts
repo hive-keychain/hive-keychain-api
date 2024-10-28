baed1eedb948ee80a919c40f8ee96ed5d347922c
stoodkev <quentin.correa@gmail.com>
2024-10-28 15:30:12 +0800

    redirect invoices

--- src/api/hive/invoice-redirect.api.ts
+++ src/api/hive/invoice-redirect.api.ts
@@ -5,7 +5,12 @@ const setupApis = (app: Express) => {
   app.get("/hive/invoice/:op", async function (req, res) {
     try {
       Logger.debug(`Redirecting to ${req.params.op}`);
-      res.status(302).redirect(`hive://sign/op/${req.params.op}`);
+      res
+        .status(302)
+        .send(
+          "If you see this page, you have not been redirected. Please open this link in your browser"
+        )
+        .redirect(`hive://sign/op/${req.params.op}`);
     } catch (e) {
       res.status(500).send(e);
     }
