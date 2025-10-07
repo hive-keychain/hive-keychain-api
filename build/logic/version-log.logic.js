ffd9def50214d02ae485ac46cc8ee741213b5e80
stoodkev <quentin.correa@gmail.com>
2025-10-07 12:06:35 +0800

    fix ts errors

--- build/logic/version-log.logic.js
+++ build/logic/version-log.logic.js
@@ -15,31 +15,48 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.VersionLogLogic = void 0;
-var fs = __importStar(require("fs"));
-var getLastExtensionVersion = function (mobile) {
-    var filename = mobile ? "mobile.json" : "extension.json";
-    return fs.readFileSync(__dirname + "/../../version/".concat(filename));
+exports.VersionLogLogic = exports.VersionType = void 0;
+const fs = __importStar(require("fs"));
+const path_1 = __importDefault(require("path"));
+var VersionType;
+(function (VersionType) {
+    VersionType["MOBILE"] = "mobile";
+    VersionType["EXTENSION"] = "extension";
+})(VersionType || (exports.VersionType = VersionType = {}));
+const getLastExtensionVersion = (versionType) => {
+    return fs.readFileSync(path_1.default.join(__dirname, `/../../json/version/${versionType}.json`));
 };
-var setLastExtensionVersion = function (json, mobile) {
+const setLastExtensionVersion = (json, versionType) => {
     try {
-        var filename = mobile ? "mobile.json" : "extension.json";
-        fs.writeFile(__dirname + "/../../version/".concat(filename), JSON.stringify(json), "utf8", function () { return console.log("Version updated to ".concat(json.version)); });
+        fs.writeFile(path_1.default.join(__dirname, `/../../json/version/${versionType}.json`), JSON.stringify(json), "utf8", () => console.log(`Version updated to ${json.version}`));
     }
     catch (err) {
         console.log(err);
     }
 };
 exports.VersionLogLogic = {
-    setLastExtensionVersion: setLastExtensionVersion,
-    getLastExtensionVersion: getLastExtensionVersion,
+    setLastExtensionVersion,
+    getLastExtensionVersion,
 };
 //# sourceMappingURL=version-log.logic.js.map
