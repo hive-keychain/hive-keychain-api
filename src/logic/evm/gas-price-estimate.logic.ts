4c4ecc5f11dbd1c3b4b9c36b70a8f5939afbc94c
Cedric Gui <cedric.guillas78@gmail.com>
2026-03-02 11:06:57 +0800

    clear code

--- src/logic/evm/gas-price-estimate.logic.ts
+++ src/logic/evm/gas-price-estimate.logic.ts
@@ -1,39 +1,7 @@
 import { EvmLightNodeLogic } from "./light-node.logic";
 
-interface ChainGasFees {
-  chainId: string;
-  updatedAt: number;
-  data: GasFees;
-}
-
-interface GasFees {
-  low: GasFeeData;
-  medium: GasFeeData;
-  high: GasFeeData;
-  estimatedBaseFee: string;
-  networkCongestion: number;
-  latestPriorityFeeRange: string[];
-  historicalPriorityFeeRange: string[];
-  historicalBaseFeeRange: string[];
-  priorityFeeTrend: string;
-  baseFeeTrend: string;
-}
-
-interface GasFeeData {
-  suggestedMaxPriorityFeePerGas: string;
-  suggestedMaxFeePerGas: string;
-  minWaitTimeEstimate: number;
-  maxWaitTimeEstimate: number;
-}
-
-const Auth = Buffer.from(
-  process.env.INFURA_API_KEY + ":" + process.env.INFURA_API_KEY_SECRET,
-).toString("base64");
-
 const getGasFeesFromLightNode = async (chainId: string) => {
-  const data = await EvmLightNodeLogic.getGasFee(chainId);
-  console.log("data", data);
-  return data;
+  return await EvmLightNodeLogic.getGasFee(chainId);
 };
 
 export const GasPriceEstimateLogic = {
